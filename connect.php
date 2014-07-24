ae87ecbef23bacb6b6a05bdd376927e3b33a6ab1
Felix Wong <felix@fawong.com>
2014-07-24 07:01:28 +0000

    better error on db fail

--- connect.php
+++ connect.php
@@ -2,6 +2,13 @@
 include_once('includes/ezSQL/shared/ez_sql_core.php');
 include_once('includes/ezSQL/mysqli/ez_sql_mysqli.php');
 
+function eh($errno, $errstr) {
+    http_response_code(500);
+    header("Not-A-CMS-Fail: $errstr");
+    die("Failed to connect to MySQL database");
+}
+set_error_handler('eh');
+
 // Connect to database
 $db = new ezSQL_mysqli("$db_username", "$db_password", "$db_name", "$db_host");
 
