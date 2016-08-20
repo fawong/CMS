a5b8ab8d5b77c9875745f1a696d38ef499762e78
Felix Wong <felix@fawong.com>
2016-08-20 07:10:00 +0000

    update sql file

--- mysql.sql
+++ mysql.sql
@@ -18,152 +18,163 @@ CREATE TABLE `calendar` (
   `end_date` date NOT NULL,
   `start_time` time NOT NULL,
   `end_time` time NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `description` text NOT NULL,
-  `category` text NOT NULL,
-  `event_key` tinytext NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `description` mediumtext NOT NULL,
+  `category` mediumtext NOT NULL,
+  `event_key` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `category` (
   `id` smallint(6) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL DEFAULT '',
-  `description` varchar(255) NOT NULL DEFAULT '',
-  `hidden` varchar(255) NOT NULL DEFAULT '0',
-  `password` varchar(255) NOT NULL DEFAULT '',
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `description` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `hidden` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `comments` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `post_id` varchar(255) NOT NULL,
+  `post_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `author_id` int(11) NOT NULL,
-  `comment` text NOT NULL,
+  `comment` mediumtext NOT NULL,
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
-  KEY `id` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+  KEY `post_id` (`post_id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `files` (
   `id` smallint(6) NOT NULL AUTO_INCREMENT,
-  `user_id` varchar(255) NOT NULL DEFAULT '',
-  `name` varchar(255) NOT NULL DEFAULT '',
-  `path` varchar(255) NOT NULL DEFAULT '',
-  `type` varchar(255) NOT NULL DEFAULT '',
-  `num_downloads` varchar(255) NOT NULL DEFAULT '0',
+  `user_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `path` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `type` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `num_downloads` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `date_submitted` date NOT NULL,
-  `description` varchar(255) NOT NULL DEFAULT '',
+  `description` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `hidden` tinyint(1) NOT NULL DEFAULT '0',
-  `category` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL DEFAULT '',
+  `category` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=22352 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=22352 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `groups` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` tinytext NOT NULL,
-  `description` text NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `description` mediumtext NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`(20))
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `links` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `url` varchar(255) NOT NULL,
-  `target` varchar(10) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+  `url` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `target` varchar(10) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `url` (`url`),
+  KEY `name` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `pages` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `page_name` varchar(255) NOT NULL DEFAULT '',
-  `page_title` varchar(255) NOT NULL DEFAULT '',
+  `page_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `page_title` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `author_id` int(11) NOT NULL,
-  `header` text NOT NULL,
-  `body` mediumtext NOT NULL,
-  `footer` text NOT NULL,
-  `css` text NOT NULL,
+  `header` mediumtext NOT NULL,
+  `body` longtext NOT NULL,
+  `footer` mediumtext NOT NULL,
+  `css` mediumtext NOT NULL,
   `views` mediumint(9) NOT NULL,
   `hidden` tinyint(1) NOT NULL DEFAULT '0',
   `admin` tinyint(1) NOT NULL DEFAULT '1',
   `member` tinyint(1) NOT NULL,
   `public` tinyint(1) NOT NULL,
-  `password` varchar(255) NOT NULL DEFAULT '',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9360 DEFAULT CHARSET=latin1;
+  `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `page_name` (`page_name`),
+  KEY `page_title` (`page_title`)
+) ENGINE=InnoDB AUTO_INCREMENT=9360 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `personal_messages` (
   `id` int(255) NOT NULL AUTO_INCREMENT,
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `subject` varchar(255) NOT NULL DEFAULT '',
+  `subject` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `from_user_id` int(11) NOT NULL,
   `to_user_id` int(11) NOT NULL,
-  `text` mediumtext NOT NULL,
+  `text` longtext NOT NULL,
   `mark_read` tinyint(1) NOT NULL DEFAULT '0',
   `folder` tinyint(4) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=50 DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`id`),
+  KEY `from_to` (`from_user_id`,`to_user_id`) USING BTREE,
+  KEY `folder` (`folder`)
+) ENGINE=InnoDB AUTO_INCREMENT=53 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `posts` (
   `id` int(255) NOT NULL AUTO_INCREMENT,
   `author_id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `post` mediumtext NOT NULL,
+  `title` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `post` longtext NOT NULL,
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=74 DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`id`),
+  KEY `title` (`title`)
+) ENGINE=InnoDB AUTO_INCREMENT=76 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `settings` (
-  `setting` varchar(255) NOT NULL,
-  `value` varchar(255) NOT NULL,
-  PRIMARY KEY (`setting`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `id` int(11) NOT NULL,
+  `setting` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `value` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `setting` (`setting`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `first_name` varchar(255) NOT NULL,
-  `last_name` varchar(255) NOT NULL,
-  `username` varchar(255) NOT NULL DEFAULT '',
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `first_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `last_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `username` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `group_id` tinyint(4) NOT NULL DEFAULT '2',
-  `password` varchar(255) NOT NULL,
+  `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `date_joined` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `date_of_birth` date NOT NULL,
-  `email` varchar(255) NOT NULL DEFAULT '',
+  `email` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `ip` int(11) NOT NULL,
-  `last_log_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `activation_code` varchar(15) NOT NULL,
+  `last_log_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `activation_code` varchar(15) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `activated_account` tinyint(1) NOT NULL,
   `online` tinyint(1) NOT NULL,
-  `about` text NOT NULL,
-  `website` varchar(255) NOT NULL,
-  `interests` text NOT NULL,
-  `aim` varchar(255) NOT NULL,
-  `msn` varchar(255) NOT NULL,
-  `yahoo` varchar(255) NOT NULL,
-  `avatar` text NOT NULL,
-  `signature` text NOT NULL,
-  `theme` varchar(255) NOT NULL DEFAULT 'default',
+  `about` mediumtext NOT NULL,
+  `website` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `interests` mediumtext NOT NULL,
+  `aim` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `msn` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `yahoo` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `avatar` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `signature` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `theme` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `kpos` smallint(6) NOT NULL DEFAULT '0',
   `kneg` smallint(6) NOT NULL DEFAULT '0',
   `access_file_manager` tinyint(1) NOT NULL DEFAULT '0',
@@ -171,9 +182,11 @@ CREATE TABLE `users` (
   `banned` tinyint(1) NOT NULL DEFAULT '0',
   `views` mediumint(9) NOT NULL,
   `access_dlcp` tinyint(1) NOT NULL DEFAULT '0',
+  `last_modified_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=98898 DEFAULT CHARSET=latin1;
+  UNIQUE KEY `username` (`username`),
+  KEY `name` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=98898 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
